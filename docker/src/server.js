3545c8e85971649e5313d7294df33ae41f87ef0e
SaadSOA <saad.abdullahi@hotmail.co.uk>
2024-08-27 17:41:10 +0100

    updating main

--- docker/src/server.js
+++ docker/src/server.js
@@ -5,6 +5,6 @@ app.get('/', (req,res)=>{
      res.send("Welcome to my awesome app!"); 
  });
 
-app.listen(3000, function () {
-    console.log("app listening on port 3000");
+app.listen(3100, function () {
+    console.log("app listening on port 3100");
 });
